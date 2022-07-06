b088b9b6e7d03abb6dc0922aaa339a27ae908429
Artem Derevenets <artyomderevenets@gmail.com>
2022-07-06 16:34:07 +0300

    New env vars support in `config.yaml` and cleaner `httpd.py`

--- httpd.py
+++ httpd.py
@@ -1,20 +1,14 @@
-import os
 from wsgiref.simple_server import make_server
+
+from dotenv import load_dotenv
+
 from evernotebot.app import EvernoteBotApplication
 
+load_dotenv()  # take environment variables from .env.
 
-host = '127.0.0.1'
-port = 8080
-env_vars = (
-    'TELEGRAM_API_TOKEN',
-    'EVERNOTE_BASIC_ACCESS_KEY',
-    'EVERNOTE_BASIC_ACCESS_SECRET',
-    'EVERNOTE_FULL_ACCESS_KEY',
-    'EVERNOTE_FULL_ACCESS_SECRET',
-)
+host, port = '0.0.0.0', 8080
 app = EvernoteBotApplication()
-for name in env_vars:
-    os.environ[name] = 'debug'
+
 try:
     with make_server(host, port, app) as httpd:
         print(f'Starting `evernotebot` at http://{host}:{port}...')
