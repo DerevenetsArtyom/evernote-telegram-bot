b334d03486f118955f4e19e68a2303162e55cb26
Artem Derevenets <derevenetsartyom@gmail.com>
2022-10-23 12:55:58 +0300

    Update rest of the files according to the chages

--- evernotebot-install.sh
+++ evernotebot-install.sh
@@ -5,6 +5,48 @@ read_env_variable() {
 	echo "export $1=\"$value\"" >> "$env_file"
 }
 
+create_env() {
+	# $1 - install_dir, $2 - env_file
+	echo "export EVERNOTEBOT_DIR=\"$install_dir\"" >> "$env_file"
+	read_env_variable "EVERNOTEBOT_DEBUG"
+	read_env_variable "EVERNOTEBOT_HOSTNAME"
+	read_env_variable "EVERNOTEBOT_EXPOSE_PORT"
+
+	read_env_variable "TELEGRAM_BOT_NAME"
+	read_env_variable "TELEGRAM_API_TOKEN"
+
+	read_env_variable "EVERNOTE_READONLY_KEY"
+	read_env_variable "EVERNOTE_READONLY_SECRET"
+
+	read_env_variable "EVERNOTE_READWRITE_KEY"
+	read_env_variable "EVERNOTE_READWRITE_SECRET"
+
+	source "$env_file"
+	echo "source $env_file" >> ~/.bashrc
+	echo "A line \"source $env_file\" added to your .bashrc"
+}
+
+# Check current user in docker group
+OS="$(uname -s)"
+if [ "$OS" = "Linux" ]; then
+	groups | grep docker > /dev/null
+	status=$?
+	if [ $status -ne 0 ]; then
+		echo "User $USER must be in group docker"
+		exit 1
+	fi
+else
+	if [ "$OS" = "Darwin" ]; then
+		groups | grep staff > /dev/null
+		status=$?
+		if [ $status -ne 0 ]; then
+			echo "User $USER must be in group staff"
+			exit 1
+		fi
+	fi
+fi
+
+# Set up installation directory
 current_dir=$(pwd)
 read -rp "Install directory (default: $current_dir): " install_dir
 if [ ! "$install_dir" ]; then
@@ -12,11 +54,13 @@ if [ ! "$install_dir" ]; then
 else
 	mkdir -p "$install_dir"
 fi
+cd "$install_dir"
 echo "Installation directory: $install_dir"
 export EVERNOTEBOT_DIR="$install_dir"
 
 docker pull djudman/evernote-telegram-bot
 
+# Create docker volume for bot data
 VOLUME_NAME="evernotebot-data"
 
 docker volume inspect $VOLUME_NAME > /dev/null 2>&1
@@ -28,16 +72,11 @@ else
 	echo "Volume $VOLUME_NAME already exists"
 fi
 
-curl https://raw.githubusercontent.com/djudman/evernote-telegram-bot/master/init.d/evernotebot --output ./evernotebot-init.d
-chown root: ./evernotebot-init.d
-chmod a+x ./evernotebot-init.d
-mv ./evernotebot-init.d /etc/init.d/evernotebot
-status=$?
-if [ $status -ne 0 ]; then
-	echo "Installation FAILED."
-	exit 1
-fi
+# Get an script for start/stop/update from github
+curl https://raw.githubusercontent.com/djudman/evernote-telegram-bot/master/init.d/evernotebot --output ./evernotebot
+chmod u+x ./evernotebot
 
+# Build `.env` file in installation directory. This file will contain environment variables
 env_file="$install_dir/.env"
 if [ -f "$env_file" ]; then
 	read -rp "Env file $env_file already exists. Do you want to rewrite it? (Y/n) " delete
@@ -45,32 +84,11 @@ if [ -f "$env_file" ]; then
 		old_env_file="$env_file.bak"
 		mv "$env_file" "$old_env_file"
 		touch "$env_file"
-
-		echo "export EVERNOTEBOT_DIR=\"$install_dir\"" >> "$env_file"
-		read_env_variable "EVERNOTEBOT_DEBUG"
-		read_env_variable "EVERNOTEBOT_HOSTNAME"
-
-		read_env_variable "TELEGRAM_BOT_NAME"
-		read_env_variable "TELEGRAM_API_TOKEN"
-
-		read_env_variable "EVERNOTE_BASIC_ACCESS_KEY"
-		read_env_variable "EVERNOTE_BASIC_ACCESS_SECRET"
-
-		read_env_variable "EVERNOTE_FULL_ACCESS_KEY"
-		read_env_variable "EVERNOTE_FULL_ACCESS_SECRET"
-
-		read -rp "Would you like to use mongodb as a storage (default: sqlite)? (y/N) " use_mongo
-		if [ "$use_mongo" = "y" ]; then
-			read_env_variable "MONGO_HOST"
-		fi
-
-		echo "source $env_file" >> ~/.bashrc
-		echo "A line \"source $env_file\" added to your .bashrc"
-
-		rm -f "$old_env_file"
-		echo "File $old_env_file deleted"
+		create_env
 	fi
+else
+	create_env
 fi
 
 echo "Evernote bot successfuly installed to $install_dir"
-echo "Use /etc/init.d/evernotebot start|stop|restart to start/stop/restart bot"
+echo "Use ./evernotebot start|stop|restart|update"
--- httpd.py
+++ httpd.py
@@ -1,18 +1,33 @@
 import os
-from dotenv import load_dotenv
-from wsgiref.simple_server import make_server
 
-from evernotebot.app import EvernoteBotApplication
+import uvicorn
 
-load_dotenv()  # take environment variables from .env.
 
-host = '0.0.0.0'
-port = int(os.getenv("PORT", "5000"))
-app = EvernoteBotApplication()
+# This file uses for debug only
 
-try:
-    with make_server(host, port, app) as httpd:
+
+def set_var(name, value):
+    os.environ[name] = value
+
+
+if __name__ == '__main__':
+    map(set_var, (
+        # We have to define some required env vars
+        ('TELEGRAM_API_TOKEN', 'debug'),
+        ('EVERNOTE_READONLY_KEY', 'debug'),
+        ('EVERNOTE_READONLY_SECRET', 'debug'),
+        ('EVERNOTE_READWRITE_KEY', 'debug'),
+        ('EVERNOTE_READWRITE_SECRET', 'debug'),
+    ))
+
+    host = '127.0.0.1'
+    port = 8000
+    try:
         print(f'Starting `evernotebot` at http://{host}:{port}...')
-        httpd.serve_forever()
-except KeyboardInterrupt:
-    app.shutdown()
+        config = uvicorn.Config('evernotebot.wsgi:app', host=host, port=port, log_level='debug')
+        server = uvicorn.Server(config)
+        server.run()
+    except KeyboardInterrupt:
+        pass
+        # TODO:
+        # app.shutdown()
